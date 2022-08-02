738e0d151d01286a4d54cd1eb4d86e2440e663c4
AceyGit <daniel.aceheaton@hotmail.com>
2022-08-02 22:00:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myfirstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        private EditText Number1;
+        private EditText Number2;
+        private Button plus;
+        private Button minus;
+        private Button times;
+        private Button divide;
+
+
+    }
+}
